e83c84f9ed8c4a0ccd9200f7cc69807828b7e9b9
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2025-08-09 18:17:36 +0600

    Update main.dart

--- Log in demo page/main.dart
+++ Log in demo page/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart'; // gets material library of flutter.
 
+import 'package:firebase_core/firebase_core.dart'; // imports firebase_core package
+
 import 'log_in_page_dart.dart'; // gets the log_in_page_dart file codes.
 
-void main () {
+void main () async { // async tells the function might contain operations that won't complete immediately and that it will return a Future
+
+  WidgetsFlutterBinding.ensureInitialized(); // ✅ needed for async main. To connect flutter with underlying platform
+  await Firebase.initializeApp(); // ✅ initializes Firebase here. Needed to be called before using Firebase services.
 
   runApp( MyApp()); // runApp() starts the app from this widget called MyApp.
 
@@ -16,8 +21,8 @@ class MyApp extends StatelessWidget{ // defines root widget MyApp which is state
   Widget build (BuildContext context){ // every widget must define a build method that returns what the UI
     // should look like and context gives the info where the widget is in the widget tree.
 
-    return MaterialApp( // starts with Material design it is a root widget that sets app-wide
-      // features like themes, title & navigation.
+    return MaterialApp( /* starts with Material design it is a root widget that sets app-wide
+       features like themes, title & navigation. */
 
       title: 'Flutter log in demo', // sets the title or name of the app visible in app switcher.
 
@@ -33,8 +38,8 @@ class MyApp extends StatelessWidget{ // defines root widget MyApp which is state
 
       ),
 
-      home: LoginPage() // This tells Flutter to show the LoginPage widget (which I imported earlier)
-      // when the app starts.
+      home: LoginPage() /* This tells Flutter to show the LoginPage widget (which I imported earlier)
+       when the app starts. */
 
     );
 
